b6b1946a00933b8408af971f466696cb15f6e978
neeasade <nathanisom27@gmail.com>
2015-04-29 03:51:03 -0500

    Allow info.sh to take arguments for what to display

--- home/bar/.config/bar/info.sh
+++ home/bar/.config/bar/info.sh
@@ -4,7 +4,6 @@
 # TODO: If Verbose_bar has a value in bar/profile, will output more information
 
 # default location for a battery capacity
-export BATC=/sys/class/power_supply/BAT0/capacity
 
 clock() {
     date '+%b%e,%l:%M'
@@ -12,13 +11,18 @@ clock() {
 
 battery() {
     # if we reach here is assumed this computer has a battery.
+    BATC=/sys/class/power_supply/BAT0/capacity
     BATS=/sys/class/power_supply/BAT0/status
-    test "`cat $BATS`" = "Charging" && echo -n '+' || echo -n '-'
-    sed -n p $BATC
+    if [ -f $BATC ]; then
+        test "`cat $BATS`" = "Charging" && echo -n '+' || echo -n '-'
+        sed -n p $BATC
+    else
+        echo '+100'
+    fi
 }
 
 volume() {
-    amixer get Master | sed -n 's/^.*\[\([0-9]\+%\).*$/\1/p'
+    echo "vol $(amixer get Master | sed -n 's/^.*\[\([0-9]\+%\).*$/\1/p')"
 }
 
 network() {
@@ -31,24 +35,31 @@ network() {
         eth0=$int1
     fi
     ip link show $eth0 | grep 'state UP' >/dev/null && int=$eth0 ||int=$wifi
-
+    echo -n "⇅ "
     ping -c 1 8.8.8.8 >/dev/null 2>&1 &&
         echo "✔" || echo "✖"
 }
 
 # The {E} bar command below provides a slant from this fork: http://github.com/neeasade/bar
-delim="${pBGS1}%{E${pSLANT}}$(printf %${pSLANT}s)${pFG}"
-delim2="${pBGS2}%{E${pSLANT}}$(printf %${pSLANT}s)${pFG}"
+delim=" ${pBGS1}%{E${pSLANT}}$(printf %${pSLANT}s)${pFG} "
+delim2=" ${pBGS2}%{E${pSLANT}}$(printf %${pSLANT}s)${pFG} "
 
+#determine what to display based on arguments, unless there are none, then display all.
 while :; do
-    buf="S$delim2"
-    if [ -f $BATC ]; then
-        buf="${buf} $(battery) $delim "
+    if [ -z "$*" ];then
+        buf="S"
+        buf="${buf}${delim2}$(battery)"
+        buf="${buf}${delim}$(network)"
+        buf="${buf}${delim2}$(volume)"
+        buf="${buf}${delim}$(clock)"
     else
-        buf="${buf} ⇅ $(network) $delim "
+        cur_delim="$delim2"
+        buf="S"
+        for arg in "$*"; do
+            buf="${buf} $cur_delim $($arg) "
+            [ "$cur_delim" = "$delim" ] && cur_delim="$delim2" || cur_delim="$delim"
+        done
     fi
-    buf="${buf} vol $(volume) $delim2 "
-    buf="${buf} $(clock)"
 
     echo "$buf"
     sleep 1 # The HUD will be updated every second
