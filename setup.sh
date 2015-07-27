e68c2eaeadda40827621427ef12ebc866404ce1e
Nathan Isom <nathanisom27@gmail.com>
2015-07-26 23:54:34 -0500

    folder restructure.
    Former-commit-id: 9b50a546cea5a5681300c96b0975db638e5fbdd0

--- setup.sh
+++ setup.sh
@@ -3,13 +3,7 @@
 git submodule init
 
 # Install the depends.txt programs using yaourt:
-
-for i in $(ls -d */ | grep -v "conflict"); do
-    cd $i
-        echo "Installing from ${i}depends.txt:"
-        for package in $(cat ./depends.txt); do
-            yaourt -S $package --needed
-        done
-    cd ..
+for package in $(cat ./depends.txt); do
+    yaourt -S $package --needed --noconfirm
 done
 
