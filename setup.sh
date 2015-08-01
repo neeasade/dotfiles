48a57796c823e886fb02cbe3909cf9ad90e82f56
Nathan Isom <nathanisom27@gmail.com>
2015-08-01 13:24:19 -0500

    Update setup.sh and depends.txt
    Former-commit-id: b24df78322bccf87054601e81ba0d5cd4b9dedcd
    Former-commit-id: bdbb44aef83e5d0dd16e6155de77170bc918107b

--- setup.sh
+++ setup.sh
@@ -1,6 +1,17 @@
 #!/bin/sh
+# Spaghetti code makes the world go round.
 
 git submodule init
+
+cd ./bin/bin/.colort
+make
+
+cd ../../..
+
+cd ./bin/bin/.gtkreload
+make
+
+cd ../../..
 
 # Install the depends.txt programs using yaourt:
 for package in $(cat ./depends.txt); do
