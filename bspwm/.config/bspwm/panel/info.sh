16fac1d61be525faa5f06bdb5d39724d857b6d81
internaught <scott@internaught.io>
2015-08-02 14:15:08 -0500

    cleaning up stuff

--- bspwm/.config/bspwm/panel/info.sh
+++ bspwm/.config/bspwm/panel/info.sh
@@ -15,12 +15,13 @@ icon() {
 weather() {
     icon f0c2
     weatherURL='http://www.accuweather.com/en/us/manhattan-ks/66502/weather-forecast/328848'
-    wget -q -T 1 -O- "$weatherURL" | awk -F\' '/acm_RecentLocationsCarousel\.push/{print $12"°F" }'| head -1
+    wget -q -T 1 -O- "$weatherURL" | awk -F\' '/acm_RecentLocationsCarousel\.push/ {print $12"°F"; exit; }'
 }
 
 clock() {
     icon f017
-    date '+%l:%M %p'
+    # i want to see the day, month
+    date '+%a, %b,%e %l:%M %p'
     # todo: gcal popup here
 }
 
@@ -31,12 +32,12 @@ mail() {
 }
 
 battery() {
-    BATC=/sys/class/power_supply/BAT0/capacity
-    BATS=/sys/class/power_supply/BAT0/status
+    batc=/sys/class/power_supply/BAT0/capacity
+    bats=/sys/class/power_supply/BAT0/status
     icon f0e7
-    if [ -f $BATC ]; then
-        [ "`cat $BATS`" = "Charging" ] && echo -n '+' || echo -n '-'
-        cat $BATC
+    if [ -f $batc ]; then
+        [ "`cat $bats`" = "Charging" ] && echo -n '+' || echo -n '-'
+        cat $batc
     else
         #no battery information found.
         echo '+100'
@@ -69,18 +70,18 @@ mpd() {
     cur_song=$(basename "$(mpc current)" | sed "s/^\(.*\)\..*$/\1/" | cut -c1-30 )
 
     icon f025
-    if [ -z "$cur_song" ]; then
+    if [[ -z "$cur_song" ]]; then
         echo "Stopped"
     else
         paused=$(mpc | grep paused)
-        [ -z "$paused" ] && toggle="${AC}mpc pause${AB}$(icon f04c)${AE}" ||
+        [[ -z "$paused" ]] && toggle="${AC}mpc pause${AB}$(icon f04c)${AE}" ||
             toggle="${AC}mpc play${AB}$(icon f04b)${AE}"
         prev="${AC}mpc prev${AB}$(icon f049)${AE}"
         next="${AC}mpc next${AB}$(icon f050)${AE}"
         echo "$cur_song  $prev $toggle $next"
     fi
 }
-
+# This thing eats up CPU cycles
 yaourtUpdates() {
     updates=$(eval yaourt -Qu | wc --lines)
     command='termite -e "yaourt -Syua"'
@@ -90,7 +91,7 @@ yaourtUpdates() {
 themeSwitch() {
     # ghetto
     # todo: replace with dzen dropdown to click themes from dir.
-    cur_theme=$(cat ~/.bspwm_theme | grep THEME_NAME | cut -c12-)
+    cur_theme=$(grep "THEME_NAME" ~/.bspwm_theme | cut -c12-)
     case $cur_theme in
         pyonium) next_theme=twilight;;
         twilight) next_theme=zenburn;;
@@ -109,7 +110,7 @@ blockActive=false;
 while :; do
     buf="S"
 
-    [ -z "$*" ] && items="mail yaourtUpdates mpd battery network volume weather clock themeSwitch" \
+    [[ -z "$*" ]] && items="mail yaourtUpdates mpd battery network volume weather clock themeSwitch" \
                 || items="$@"
 
     for item in $items; do
