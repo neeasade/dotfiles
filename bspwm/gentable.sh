4e534498fadbeb3b18472c69b9a1c513b918248f
Nathan Isom <nathanisom27@gmail.com>
2015-12-29 17:15:44 -0800

    todo/ autogenerate table with notes.
    Former-commit-id: 1525e15609dd53511790c231ad8f5806a57c0588
    Former-commit-id: b65456c8150d8cbce3796ff398e6f1167d62a21e

---
+++ bspwm/gentable.sh
@@ -0,0 +1,28 @@
+#!/bin/env bash
+
+# generate a markdown table from a theme file.
+
+echo "variable | default | note"
+echo "----|----|-"
+
+IFS=$'\r\n'
+while read line; do
+	if [[ ! -z $line ]]; then
+		if echo "$line" | grep -q "=" ; then
+		if ! echo "$line" | grep -q "\[" ; then
+			line=$(sed 's/export //' <<< "$line")
+			name=$(echo "$line" | sed 's/=.*//')
+			value=$(echo "$line" | sed 's/[^=]+=//;s/ #.*//')
+			if echo "$line" | grep -q " #"; then
+				desc=$(echo "$line" | sed 's/.* #//' | cut -c2-)
+			else
+				desc="none"
+			fi
+			echo "\`$name\` | \`$value\` | $desc"
+			name=
+			value=
+			desc=
+		fi
+		fi
+	fi
+done<"$1"
