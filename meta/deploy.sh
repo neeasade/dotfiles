836f095eaacd93d7421b1e6b9355990c96225ad3
Nathan Isom <nathanisom27@gmail.com>
2016-01-22 18:15:17 -0600

    make deploy work with current scheme.
    Former-commit-id: 756319338328e373a59a3a751e6cef363c64a96e
    Former-commit-id: 2ba37f2f5a2e94c321516e2188da546c310a1133

--- meta/deploy.sh
+++ meta/deploy.sh
@@ -1,33 +1,33 @@
 #!/bin/bash
 # Deploy these dotfiles to your setup.
 
-cd $(dirname $0)
+cd $(dirname $0)/..
 
 # if run with no arguments, setup packages/submodules/scripts.
 if [ -z "$@" ]; then
     git submodule init
 
-    cd ./bin/bin/.colort
+    cd wm/.wm/scripts/.colort
     make
     cd ../.gtkreload
     make
     cd ../../..
 
-    for package in $(cat ./depends.txt); do
+    for package in $(cat meta/depends.txt); do
         yaourt -S $package --needed --noconfirm
     done
 fi
 
 echo "backing up any conflicts to ~/dotfile_conflicts.."
 IFS=$'\n'
-for file in $(stow -n $(ls */ -d | grep -v root) 2>&1 | grep -oE ":.+" | cut -c3-); do
+for file in $(stow -n */ 2>&1 | grep -oE ":.+" | cut -c3-); do
     mkdir -p ~/dotfile_conflicts/$(dirname $file)
     mv ~/$file ~/dotfile_conflicts/$file
     echo $file
 done
 
 echo "Linking dotfiles to home dir.."
-stow $(ls */ -d | grep -v root)
+stow */
 
-$HOME/bin/ltheme twilight
+$HOME/.wm/scripts/ltheme twilight
 echo "All done."
