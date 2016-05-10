90bcbb8bffd31e4c50cdc4e900386d747a3152ff
Nathan Isom <nathanisom27@gmail.com>
2016-05-09 20:33:42 -0500

    misc improvements due to new query abilities, also do funky external ruling.
    Former-commit-id: 910de9061909babc9c60566185646452f34fe40f
    Former-commit-id: 706aa8dac3d06d37a4af343de7b50174fcdb9128

--- wm/.wm/scripts/external_rules.sh
+++ wm/.wm/scripts/external_rules.sh
@@ -6,6 +6,13 @@
 # if there are any presel's on current desktop, spawn there.
 # else, spawn a prefered split in current node based on node size.
 
+# preferred split directions:
+horiPref=east
+vertPref=south
+# if dimensions are within this percent, will split $horiPref,
+# else direction is determined by width or height being greater.
+percent=.20
+
 # get any presels on the current desktop, select one if so.
 presel="$(bspc query -N -d -n .\!automatic | head -n 1)"
 targetNode=${presel:-focused}
@@ -22,5 +29,13 @@ echo node=$targetNode
 [ ! -z $presel ] && exit 0
 
 # determine split dir on focused node with preferred direction.
-[ $width -gt $height ] && split_dir=east || split_dir=south
+[ $width -gt $height ] && bcInput="($width-$height)/$width" || bcInput="($height-$width)/$height"
+result=$(echo "$bcInput < $percent" | bc -l)
+
+if [ "$result" -eq "1" ]; then
+    split_dir=$vertPref
+else
+    [ $width -gt $height ] && split_dir=east || split_dir=south
+fi
+
 echo split_dir=$split_dir
--- wm/.wm/scripts/pulse.sh
+++ wm/.wm/scripts/pulse.sh
@@ -1,19 +1,22 @@
 #!/usr/bin/env dash
-# use wmutils to pulse colors based around current bspwm color
+# neeasade
+# depends on: colort, wmutils
+# pulse colors based around current bspwm color
 # modified from the wmutils contrib repo.
-# depends on bspwm and colort
 
 hash chwb 2>/dev/null || { echo >&2 "$0 requires wmutils."; exit 1;  }
 
 FREQ=0.07
-NORMAL_COLOR="$(bspc config normal_border_color | cut -c2- )"
+NORMAL_COLOR="$(bspc config normal_border_color | tr -d \#)"
 
-COLORS="$(bspc config active_border_color | cut -c2- )"
+COLORS="$(bspc config active_border_color | tr -d \#)"
 LAST_COLOR="$COLORS"
 
-colort -c "$LAST_COLOR" && set="20 20 20 20 20 20 -20 -20 -20 -20 -20 -20" || \
-                           set="-20 -20 -20 -20 -20 -20 20 20 20 20 20 20"
+colort -c "$LAST_COLOR" && \
+    set="20 20 20 20 20 20 -20 -20 -20 -20 -20 -20" || \
+    set="-20 -20 -20 -20 -20 -20 20 20 20 20 20 20"
 
+IFS=' '
 for i in $set; do
     NEW_COLOR="$LAST_COLOR"
     LAST_COLOR=$(colort -l $i "$LAST_COLOR")
