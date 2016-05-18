00a02ea14dba1ff3f93072d30478903a0f9df129
Nathan Isom <nathanisom27@gmail.com>
2016-05-17 20:30:54 -0500

    sxhkd-git, resize.sh corrections.
    Former-commit-id: bccae7abc8dacc078157db849945be42083773ff
    Former-commit-id: 27e49b0ed04366feb99f2b37a27d6d5fb8192778

--- wm/.wm/scripts/resize.sh
+++ wm/.wm/scripts/resize.sh
@@ -6,15 +6,15 @@ percent=3
 # get rectangle property of origin node, floating or tiling (x,y,width,height)
 dim()
 {
-	bspc query -T -n $originalNode | jq ".client.$(bspc query -T -n | jq -r .client.state)Rectangle.$1"
+	bspc query -T -n $originalNode | jq ".rectangle.$1"
 }
 
 # set fall back, and target window property.
 case $1 in
-	left)   fallDir=right;  targetProp=width;  queryDir=x;  sign=-;;
-	right)  fallDir=left;   targetProp=width;  queryDir=x;;
-	top)    fallDir=bottom; targetProp=height; queryDir=y;  sign=-;;
-	bottom) fallDir=top;    targetProp=height; queryDir=y;;
+	left)   fallDir=right;  targetProp=width;  queryDir=x; op="head"; sign=-;;
+	right)  fallDir=left;   targetProp=width;  queryDir=x; op="tail";;
+	top)    fallDir=bottom; targetProp=height; queryDir=y; op="head"; sign=-;;
+	bottom) fallDir=top;    targetProp=height; queryDir=y; op="tail";;
 	*) exit 1;;
 esac
 
@@ -22,29 +22,30 @@ esac
 originalNode=$(bspc query -N -n)
 afterAction="bspc node -f $originalNode"
 if bspc query -N -n focused.\!window; then
-	targetNode=$(bspc query -T -n | jq ".. | .?, .root?, .firstChild?, .secondChild? | select (.client != null) | [.] | sort_by(.rectangle.$queryDir) | .[] | .id" | head -n 1)
+	targetNode=$(bspc query -T -n | jq ".. | .?, .root?, .firstChild?, .secondChild? | select (.client != null) | [.] | sort_by(.rectangle.$queryDir) | .[] | .id" | $op -n 1)
 	bspc node -f $targetNode
 fi
 
 # set move args
 moveArgs="$sign$(echo "$percent/100*$(bspc query -T -m | jq .rectangle.$targetProp)" | bc -l)"
-[ $targetProp = "height"  ] && moveArgs="0 $moveArgs" || moveArgs="$moveArgs 0"
+[ $targetProp = "height" ] && moveArgs="0 $moveArgs" || moveArgs="$moveArgs 0"
 
 # do the thing
 beforeVal=$(dim $targetProp)
 bspc node -z $1 $moveArgs
-[ "$beforeVal" = "$(dim $targetProp)"  ] && bspc node -z $fallDir $moveArgs
+
+[ "$beforeVal" = "$(dim $targetProp)" ] && bspc node -z $fallDir $moveArgs
 
 if [ "$beforeVal" = "$(dim $targetProp)" ]; then
 	# we were focused on a group, do funky things.
 	afterAction="$afterAction && bspc node -B"
 	case $1 in
-		left)   bspc node -f east;;
-		right)  bspc node -f west;;
-		top)    bspc node -f south;;
-		bottom) bspc node -f north;;
+		left)   bspc node -f $originalNode; bspc node -f  east;;
+		right)  bspc node -f $originalNode; bspc node -f  west;;
+		top)    bspc node -f $originalNode; bspc node -f south;;
+		bottom) bspc node -f $originalNode; bspc node -f north;;
 	esac
 	bspc node -z $1 $moveArgs
 fi
 
-$afterAction
+eval $afterAction
