fc1d554012b3b273b19ff1e14fdd522705fec985
Nathan Isom <nathanisom27@gmail.com>
2016-05-18 21:36:15 -0500

    resize.sh is now much more robust and not as ugly.
    Former-commit-id: d832eaff065533fa5fabddd7020b221b0a956d44
    Former-commit-id: 2453ccf7256cae3b4d027bbd250a3e09ebe036ca

--- wm/.wm/scripts/resize.sh
+++ wm/.wm/scripts/resize.sh
@@ -1,51 +1,52 @@
 #!/usr/bin/env dash
 # depends on: jq, bc, bspc
-# (percent of current monitor resolution to move)
+# resize windows or groups of windows in bspwm.
+# (percent of current monitor resolution to resize)
 percent=3
 
 # get rectangle property of origin node, floating or tiling (x,y,width,height)
-dim()
-{
+originalNode=$(bspc query -N -n)
+dim() {
 	bspc query -T -n $originalNode | jq ".rectangle.$1"
 }
 
 # set fall back, and target window property.
 case $1 in
-	left)   fallDir=right;  targetProp=width;  queryDir=x; op="head"; sign=-;;
-	right)  fallDir=left;   targetProp=width;  queryDir=x; op="tail";;
-	top)    fallDir=bottom; targetProp=height; queryDir=y; op="head"; sign=-;;
-	bottom) fallDir=top;    targetProp=height; queryDir=y; op="tail";;
+	left)   fallDir=right;  targetProp=width;  queryDir=x; op="min"; sign=-;;
+	right)  fallDir=left;   targetProp=width;  queryDir=x; op="max"; sign=+;;
+	top)    fallDir=bottom; targetProp=height; queryDir=y; op="min"; sign=-;;
+	bottom) fallDir=top;    targetProp=height; queryDir=y; op="max"; sign=+;;
 	*) exit 1;;
 esac
 
-# if we're focused on a group of nodes, select a window within, based on desired direction.
-originalNode=$(bspc query -N -n)
-afterAction="bspc node -f $originalNode"
-if bspc query -N -n focused.\!window; then
-	targetNode=$(bspc query -T -n | jq ".. | .?, .root?, .firstChild?, .secondChild? | select (.client != null) | [.] | sort_by(.rectangle.$queryDir) | .[] | .id" | $op -n 1)
-	bspc node -f $targetNode
-fi
+# if we're focused on a group of nodes, select a window within, leaning towards our desired direction.
+bspc query -N -n focused.\!window && targetNode=$(bspc query -T -n | jq "[recurse(.[]?) | objects | select(has(\"id\") and .client!=null)] | ${op}_by(.rectangle.$queryDir) | .id")
+targetNode=${targetNode:-focused}
 
 # set move args
 moveArgs="$sign$(echo "$percent/100*$(bspc query -T -m | jq .rectangle.$targetProp)" | bc -l)"
 [ $targetProp = "height" ] && moveArgs="0 $moveArgs" || moveArgs="$moveArgs 0"
 
-# do the thing
+# note current state, initial move attempt.
 beforeVal=$(dim $targetProp)
-bspc node -z $1 $moveArgs
+bspc node $targetNode -z $1 $moveArgs
 
-[ "$beforeVal" = "$(dim $targetProp)" ] && bspc node -z $fallDir $moveArgs
+# if we're floating, this is all that is needed.
+bspc query -N -n focused.floating && exit 0
+
+# if we weren't successful, try resizing the other way
+[ "$beforeVal" = "$(dim $targetProp)" ] && bspc node $targetNode -z $fallDir $moveArgs
 
 if [ "$beforeVal" = "$(dim $targetProp)" ]; then
-	# we were focused on a group, do funky things.
-	afterAction="$afterAction && bspc node -B"
-	case $1 in
-		left)   bspc node -f $originalNode; bspc node -f  east;;
-		right)  bspc node -f $originalNode; bspc node -f  west;;
-		top)    bspc node -f $originalNode; bspc node -f south;;
-		bottom) bspc node -f $originalNode; bspc node -f north;;
-	esac
-	bspc node -z $1 $moveArgs
-fi
+	# undo our wrong resize
+	bspc node $targetNode -z $fallDir $(echo $moveArgs | tr +- -+) &
 
-eval $afterAction
+	# attempt to jump the other direction and push into the node
+	case $1 in
+		left)   targetNode=east;;
+		right)  targetNode=west;;
+		top)    targetNode=south;;
+		bottom) targetNode=north;;
+	esac
+	bspc node $targetNode -z $1 $moveArgs &
+fi
