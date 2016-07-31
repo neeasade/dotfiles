aa949b326e200553e9e4a18fdf0b66e57332d13a
Nathan Isom <nathanisom27@gmail.com>
2016-07-30 19:15:57 -0500

    keybind changes
    Former-commit-id: b3ad88629987c845b4a78c31848a4686f6b5d2c1
    Former-commit-id: 761908652cd1af083de3d5326c949928961f3dc7

--- wm/.wm/scripts/resize.sh
+++ wm/.wm/scripts/resize.sh
@@ -3,6 +3,7 @@
 # resize windows or groups of windows in bspwm.
 # (percent of current monitor resolution to resize)
 percent=7
+dir=$1
 
 # get rectangle property of origin node, floating or tiling (x,y,width,height)
 originalNode=$(bspc query -N -n)
@@ -11,16 +12,16 @@ dim() {
 }
 
 # set fall back, and target window property.
-case $1 in
-	left)   fallDir=right;  targetProp=width;  queryDir=x; op="min"; sign=-;;
-	right)  fallDir=left;   targetProp=width;  queryDir=x; op="max"; sign=+;;
-	top)    fallDir=bottom; targetProp=height; queryDir=y; op="min"; sign=-;;
-	bottom) fallDir=top;    targetProp=height; queryDir=y; op="max"; sign=+;;
+case $dir in
+	west)   dir=left;   fallDir=right;  targetProp=width;  queryDir=x; op="min"; sign=-;;
+	east)  dir=right;  fallDir=left;   targetProp=width;  queryDir=x; op="max"; sign=+;;
+	north)    dir=top;    fallDir=bottom; targetProp=height; queryDir=y; op="min"; sign=-;;
+	south) dir=bottom; fallDir=top;    targetProp=height; queryDir=y; op="max"; sign=+;;
 	*) exit 1;;
 esac
 
 # if we're focused on a group of nodes, select a window within, leaning towards our desired direction.
-bspc query -N -n focused.\!window && targetNode=$(bspc query -T -n | jq "[recurse(.[]?) | objects | select(has(\"id\") and .client!=null)] | ${op}_by(.rectangle.$queryDir).id")
+bspc query -N -n focused.\!window > /dev/null && targetNode=$(bspc query -T -n | jq "[recurse(.[]?) | objects | select(has(\"id\") and .client!=null)] | ${op}_by(.rectangle.$queryDir).id")
 targetNode=${targetNode:-focused}
 
 # set move args
@@ -29,7 +30,7 @@ moveArgs="$sign$(echo "$percent/100*$(bspc query -T -m | jq .rectangle.$targetPr
 
 # note current state, initial move attempt.
 beforeVal=$(dim $targetProp)
-bspc node $targetNode -z $1 $moveArgs
+bspc node $targetNode -z $dir $moveArgs
 
 # if we're floating, this is all that is needed.
 bspc query -N -n focused.floating && exit 0
@@ -42,11 +43,11 @@ if [ "$beforeVal" = "$(dim $targetProp)" ]; then
 	bspc node $targetNode -z $fallDir $(echo $moveArgs | tr +- -+) &
 
 	# attempt to jump the other direction and push into the node
-	case $1 in
+	case $dir in
 		left)   targetNode=east;;
 		right)  targetNode=west;;
 		top)    targetNode=south;;
 		bottom) targetNode=north;;
 	esac
-	bspc node $targetNode -z $1 $moveArgs &
+	bspc node $targetNode -z $dir $moveArgs &
 fi
