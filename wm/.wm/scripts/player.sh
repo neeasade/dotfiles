7224b732e9ba69d9b41c96d028a12ac745292aea
Nathan Isom <nathanisom27@gmail.com>
2017-02-12 21:13:53 -0600

    better player priority
    Former-commit-id: 2f2e48ac432a84eeabc1e38d1a701c51994ca0c5
    Former-commit-id: 762d170095cc86eef9a8ea1e9815c69241803878

--- wm/.wm/scripts/player.sh
+++ wm/.wm/scripts/player.sh
@@ -1,5 +1,14 @@
 #!/usr/bin/env dash
 
-mpvc && target='mpvc' || target='mpc'
+# target is picked based on which might be playing.
+mpc | grep -q playing && mpcplaying=true || mpcplaying=false
+mpvc | grep -q playing && mpvcplaying=true || mpvcplaying=false
+
+if ! $mpvcplaying && ! $mpcplaying; then
+  mpvc >/dev/null && target='mpvc' || target='mpc'
+else
+  $mpvcplaying && target='mpvc'
+  $mpcplaying && target='mpc'
+fi
 
 $target toggle
