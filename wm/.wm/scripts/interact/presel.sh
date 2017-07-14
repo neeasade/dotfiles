aad857f8719f80967d78f073e3ca21fbbb61b032
neeasade <nathanisom27@gmail.com>
2017-07-14 17:59:54 -0500

    presel feature with opt

--- wm/.wm/scripts/interact/presel.sh
+++ wm/.wm/scripts/interact/presel.sh
@@ -1,2 +1,35 @@
 #!/usr/bin/env dash
-bspc node -p $*
+
+# if we only have one, use our border thing
+if bspc query -N -n focused.leaf && which chbpresel 2>/dev/null 1>&2; then
+    (
+        cond=true
+        while $cond; do
+            presel_win=$(xdotool search --classname "presel_feedback")
+            if [ ! -z "$presel_win" ]; then
+                cond=false
+                compton-trans -w $presel_win 0
+            fi
+        done
+    ) &
+
+    bspc node -p $*
+
+    back="$(colort -t $(bspc config focused_border_color))"
+
+    colort -c $back && \
+        forward="$(colort -t  100 $(bspc config focused_border_color))" || \
+        forward="$(colort -t -100 $(bspc config focused_border_color))"
+
+    dir="$*"
+    case $dir in
+        north) dir=1 ;;
+        south) dir=3 ;;
+        east) dir=4 ;;
+        west) dir=2 ;;
+    esac
+
+    chbpresel -c $forward -C $back -d $dir $(bspc query -N -n)
+else
+    bspc node -p $*
+fi
