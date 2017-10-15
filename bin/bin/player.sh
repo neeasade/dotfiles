441a8d1aefadeaa2bdac46d9ece0f0c3165893c7
neeasade <nathanisom27@gmail.com>
2017-10-15 14:20:19 -0500

    better player.sh logic, include mpv wrapper

--- bin/bin/player.sh
+++ bin/bin/player.sh
@@ -2,10 +2,15 @@
 
 # target is picked based on which might be playing.
 mpc  | grep -q playing && mpcplaying=true  || mpcplaying=false
-mpvc | grep -q playing && mpvcplaying=true || mpvcplaying=false
+
+if pgrep mpv > /dev/null && mpvc | grep -q playing; then
+    mpvcplaying=true
+else
+    mpvcplaying=false
+fi
 
 if ! $mpvcplaying && ! $mpcplaying; then
-  mpvc >/dev/null && target='mpvc' || target='mpc'
+  pgrep mpv > /dev/null && target='mpvc' || target='mpc'
 else
   $mpvcplaying && target='mpvc'
   $mpcplaying  && target='mpc'
