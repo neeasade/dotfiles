02b8e66133d1361576ac0a759c395b93fd0d5c1c
Nathan Isom <nathanisom27@gmail.com>
2018-04-24 14:12:01 -0500

    don't override whitespace-mode characters

--- emacs/.emacs.d/lisp/theworld.el
+++ emacs/.emacs.d/lisp/theworld.el
@@ -445,14 +445,7 @@ current major mode."
   ;; todo: fix this
   (eval-after-load 'whitespace-mode
     ;; (set-face-attribute 'whitespace-space nil :background nil)
-    (setq whitespace-display-mappings
-	  ;; all numbers are Unicode codepoint in decimal. ⁖ (insert-char 182 1)
-	  '(
-	    (space-mark 32 [183] [46]) ; 32 SPACE 「 」, 183 MIDDLE DOT 「·」, 46 FULL STOP 「.」
-	    (newline-mark 10 [182 10]) ; 10 LINE FEED
-	    (tab-mark 9 [9655 9] [92 9]) ; 9 TAB, 9655 WHITE RIGHT-POINTING TRIANGLE 「▷」
-	    ))
-    )
+	)
 
   ;; done at end so it has correct font reference
   (spaceline-compile)
