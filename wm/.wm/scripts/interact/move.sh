bee9695e9457eb00edc6ed55575ea38368bcadb8
neeasade <nathanisom27@gmail.com>
2018-08-20 17:23:32 -0500

    I hate bash

--- wm/.wm/scripts/interact/move.sh
+++ wm/.wm/scripts/interact/move.sh
@@ -4,42 +4,32 @@ bspc config pointer_follows_focus true
 dir=$1
 node=$(bspc query -N -n)
 
+case $dir in
+  west)  fdim=width; tdim=height; sign=-;;
+  east)  fdim=width; tdim=height;;
+  north) fdim=height; tdim=width; sign=-;;
+  south) fdim=height; tdim=width;;
+esac
 
 floating_move() {
-  case $dir in
-    west)  dim=width; sign=-;;
-    east)  dim=width;;
-    north) dim=height; sign=-;;
-    south) dim=height;;
-  esac
-
   percent=5
-  moveArgs="$sign$(echo "$percent/100*$(bspc query -T -m | jq .rectangle.$dim)" | bc -l)"
-  [ $dim = "height"  ] && moveArgs="0 $moveArgs" || moveArgs="$moveArgs 0"
+  moveArgs="$sign$(echo "$percent/100*$(bspc query -T -m | jq .rectangle.$fdim)" | bc -l)"
+  [ $fdim = "height" ] && moveArgs="0 $moveArgs" || moveArgs="$moveArgs 0"
   bspc node -v $moveArgs
 }
 
 tiled_move() {
-  (bspc node -n $dir.\!automatic || bspc node -s "$dir") && exit
-
-  case $dir in
-    west)  dim=height;;
-    east)  dim=height;;
-    north) dim=width;;
-    south) dim=width;;
-  esac
+  (bspc node -n $dir.\!automatic || bspc node -s "$dir") && return
 
   # compare height or width to parent
-  self_measure="$(bspc query -T -n "$node" | jq .rectangle.$dim)"
-  parent_measure="$(bspc query -T -n "${node}#@parent" | jq .rectangle.$dim)"
-  parent_measure="${parent_measure:-$self_measure}"
+  self_measure="$(bspc query -T -n "$node" | jq .rectangle.$tdim)"
+  parent_measure="$(bspc query -T -n "${node}#@parent" | jq .rectangle.$tdim)"
 
   if [ "$parent_measure" -gt "$self_measure" ]; then
-    parent="$(bspc query -N -n "${node}#@parent")"
-    bspc node $parent -p $dir
-    bspc node $parent -i
+    bspc node "${node}#@parent" -p $dir
+    bspc node "${node}#@parent" -i
 
-    receptacle_id="$(bspc query -N "${parent}#@parent" -n '.descendant_of.leaf.!window')"
+    receptacle_id="$(bspc query -N "${node}#@parent#@parent" -n '.descendant_of.leaf.!window')"
     bspc node $node -n $receptacle_id
     bspc node "${node}#@parent" -B
   else
@@ -48,10 +38,6 @@ tiled_move() {
   fi
 }
 
-if bspc query -N -n $node.floating > /dev/null; then
-  floating_move
-else
-  tiled_move
-fi
+$(bspc query -T -n | jq -r .client.state)_move
 
 bspc config pointer_follows_focus false
