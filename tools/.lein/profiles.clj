5d005d0b4fe79ad63efe4b1ec728b612689c4cb7
neeasade <neeasade@gmail.com>
2019-10-13 09:10:07 -0400

    ｓｙｎｃ

--- tools/.lein/profiles.clj
+++
@@ -1,4 +0,0 @@
-{:repl {:plugins [[cider/cider-nrepl "0.10.0-SNAPSHOT"]
-                  [refactor-nrepl "2.0.0-SNAPSHOT"]]
-        :dependencies [[alembic "0.3.2"]
-                       [org.clojure/tools.nrepl "0.2.12"]]}}
