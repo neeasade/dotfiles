ffb18faabfe350ce38f06c500975fce085826668
neeasade <neeasade@gmail.com>
2020-01-26 11:10:14 -0500

    improve meda toggle, add nfloat, mpd refresh for when mobile

--- bin/bin/player.sh
+++ bin/bin/player.sh
@@ -1,19 +1,65 @@
 #!/usr/bin/env dash
+# I want one toggle for all my media
 
-# target is picked based on which might be playing.
-mpc  | grep -q playing && mpcplaying=true  || mpcplaying=false
+dbusplaying=false
+dbuspresent=false
+if status=$(playerctl status 2>/dev/null); then
+  dbusplaying=true
 
-if pgrep mpv > /dev/null && mpvc | grep -q playing; then
-    mpvcplaying=true
-else
-    mpvcplaying=false
+  dbuspresent=true
+
+  if [ ! "$status" = "Playing" ]; then
+    # paused or stopped
+    dbusplaying=false
+  fi
+
+  if [ -z "$(playerctl metadata)" ]; then
+    dbuspresent=false
+  fi
 fi
 
-if ! $mpvcplaying && ! $mpcplaying; then
-  pgrep mpv > /dev/null && target='mpvc' || target='mpc'
+mpcplaying=$(iif 'mpc | grep -q playing')
+mpvcplaying=$(iif 'pgrep mpv > /dev/null && mpvc | grep -q playing')
+
+if ! $mpvcplaying && ! $mpcplaying && ! $dbusplaying; then
+  pgrep mpv >/dev/null && target='mpvc' || target='mpc'
+  $dbuspresent && target='playerctl'
 else
   $mpvcplaying && target='mpvc'
   $mpcplaying  && target='mpc'
+  $dbusplaying && target='playerctl'
 fi
 
+if [ "$target" = "playerctl" ]; then
+  if [ "$*" = "toggle" ]; then
+    set -- play-pause
+  elif [ "$1" = "-f" ]; then
+    # chrome xesam:album
+    # chrome xesam:artist              Vapor Memory
+    # chrome xesam:title               haircuts for men : когда зло господствует
+    metadata=$(playerctl metadata)
+    artist=$(echo "$metadata" | awk '/xesam:artist/{$1=$2=""; print $0}')
+    title=$(echo "$metadata" | awk '/xesam:title/{$1=$2=""; print $0}')
+
+    # if title has only 1 - or :, assume it's a delimiter EG yt title
+    if [ $(echo "$title" | awk -F- '{print NF-1}') -eq 1 ]; then
+      artist=$(echo "$title" | awk -F- '{print $1}')
+      title=$(echo "$title" | awk -F- '{print $2}')
+    elif [ $(echo "$title" | awk -F: '{print NF-1}') -eq 1 ]; then
+      artist=$(echo "$title" | awk -F: '{print $1}')
+      title=$(echo "$title" | awk -F: '{print $2}')
+    fi
+
+    title=$(echo "$title" | awk '{$1=$1;print}')
+    artist=$(echo "$artist" | awk '{$1=$1;print}')
+    echo "$title - $artist"
+    exit 0
+  fi
+fi
+
+if [ -z "$*" ]; then
+  set -- status
+fi
+
+# echo $target "$@"
 $target "$@"
