48b4f5d2e8f43694c66e31beb6b2ef9d789f61cc
neeasade <neeasade@users.noreply.github.com>
2021-10-12 09:36:12 -0500

    misc macos wm tweaks

--- wm/.wm/scripts/interact/focus_mac.sh
+++ wm/.wm/scripts/interact/focus_mac.sh
@@ -1,27 +1,27 @@
-#!/usr/bin/env bash
+#!/bin/sh
 
 PATH=$PATH:$HOME/bin
 
 node_dir=$1
 case $node_dir in
-  east)  dir=x; sign=-lt; emacs_dir=right ;;
-  west)  dir=x; sign=-gt; emacs_dir=left ;;
-  north) dir=y; sign=-gt; emacs_dir=up ;;
-  south) dir=y; sign=-lt; emacs_dir=down ;;
+  east)  emacs_dir=right ;;
+  west)  emacs_dir=left ;;
+  north) emacs_dir=up ;;
+  south) emacs_dir=down ;;
 esac
-
-if ! looking_at=$(yaboi query window); then
-  # we failed to see what we were looking at. assume it's a fullscreen emacs window
-  looking_at=Emacs
-else
-  looking_at=$(jq -r .app <<< "$looking_at")
-fi
 
 try_emacs_dir() {
   if timeout 0.2 elisp "(evil-window-${emacs_dir} 1) t"; then
     exit 0
   fi
 }
+
+if ! looking_at=$(yaboi query window); then
+  # we failed to see what we were looking at. assume it's a fullscreen emacs window
+  looking_at=Emacs
+else
+  looking_at=$(jget -r app "$looking_at")
+fi
 
 case $looking_at in
   Emacs) try_emacs_dir ;;
@@ -30,5 +30,5 @@ case $looking_at in
 esac
 
 yabai -m config mouse_follows_focus on
-yabai -m window --focus $node_dir
+yabai -m window --focus "$node_dir"
 yabai -m config mouse_follows_focus off
