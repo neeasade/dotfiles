eca7c7b2a33c99febf7d67f9fec7ee7e1d394cba
neeasade <neeasade@users.noreply.github.com>
2022-07-01 15:35:32 -0500

    misc updates

--- os/mac/Library/Application Support/xbar/plugins/arst.5s.sh
+++ os/mac/Library/Application Support/xbar/plugins/arst.5s.sh
@@ -7,27 +7,37 @@
 style="| font=Charter | size=12"
 style=$(theme -R '| font={{font.panel.family}} | size={{font.panel.size}}')
 
-delim=⬜
+delim=🫧
+delim='🌺'
 
 # want: time to next appt/org ical stuff
 # with org-jira, maybe dropdown links to tickets here? or in the jump tbh
 
-check_slack_dms() {
-  title=$(yaboi query windows | jq -r '.[] | select(.app == "Slack") | .title')
-  if echo "$title" | grep -q 'new item' ; then
-    echo '🔷Slack🔷'
+check_battery() {
+  battery_percent=$(pmset -g batt | awk '-F;' '/%/{print $1}' | awk '{print $NF}' | tr -d '%')
+  if [ "$battery_percent" -lt 40 ]; then
+    echo "bat: $battery_percent"
   fi
+}
+
+check_slack_dms() {
+  echo
+  # title=$(yaboi query windows | jq -r '.[] | select(.app == "Slack") | .title')
+  # if echo "$title" | grep -q 'new item' ; then
+  #   echo '🔷Slack🔷'
+  # fi
 }
 
 result=$(
     (
         check_slack_dms
-        # cache_output 500 elisp -r '(ns/org-status-outdated)'
+        check_battery
+        cache_output 30 org_task
+        cache_output 500 elisp -r '(ns/org-status-outdated)'
         # day_progress
 
-        cache_output 30 org_task
+        # echo woo
         # cache_output 30 org_task
-
     ) | awk NF | tr '\n' '^' | sed 's/.$//' | sed "s/\^/ ${delim} /g")
 
 if [ -z "$result" ]; then
