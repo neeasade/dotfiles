e670cb90df617da3c5eba5eddf29a78907739551
neeasade <neeasade@gmail.com>
2022-07-12 10:05:52 -0500

    glue gap handling with fake monocle states
    separate the knobs for gap on/off and the current view-type so these all work in
    concert now

--- wm/.wm/scripts/interact/fullscreen_toggle.sh
+++ wm/.wm/scripts/interact/fullscreen_toggle.sh
@@ -1,19 +1,10 @@
 #!/bin/sh
-# swap between fake fullscreen modes as monocle mode
-# this is so we can enjoy fake padding in fullscreen things (and account for panel presence)
-# todo: this script assumes that gaps are always what you want.
+
+gapped=$(iif '[ $(bspc config window_gap) -gt 0 ]')
 
 do_monocle_full() {
-  if pgrep picom; then
-    hsetroot -solid "$(theme -q color.normal.background)" &
-  else
-    xsetroot -solid "$(theme -q color.normal.background)" &
-  fi
-
   bspc query -N -n focused.fullscreen && \
     bspc node -t ~fullscreen
-
-  bspc node -t tiled
 
   # what are we looking at?
   window_class=$(xprop -id $(bspc query -N -n) | awk -F \" '/WM_CLASS/{print $4}')
@@ -29,35 +20,29 @@ Wine'
 
   bspc config left_monocle_padding 0
   bspc config right_monocle_padding 0
+  bspc desktop -l monocle
 
   if echo "$no_trim_list" | grep "$window_class"; then
-    gapt true 0
+    gapt false 0
   else
-    gapt true $(theme -q x.padding)
+    gapt $gapped $(theme -q x.padding)
   fi
 
-  bspc config borderless_monocle true
-  bspc desktop -l monocle
 }
 
 do_monocle_slim() {
-  ltheme bg &
-
-  bspc config borderless_monocle false
+  ltheme bg
 
   bspc query -N -n focused.fullscreen && bspc node -t ~fullscreen
   bspc node -t tiled
 
   monocle_pad_width=$(theme -q bspwm-meta.monocle-pad-width)
-
-  # todo: squish in pad-width by gap amount when no gaps (else we have thick monocle_slim)
-  # $(theme -q bspwm.window-gap)
-
   bspc config left_monocle_padding $monocle_pad_width
   bspc config right_monocle_padding $monocle_pad_width
 
-  gapt true
+  bspc config borderless_monocle false
   bspc desktop -l monocle
+  gapt $gapped
 }
 
 do_fullscreen() {
@@ -65,13 +50,12 @@ do_fullscreen() {
 }
 
 do_tiled() {
-  ltheme bg &
+  ltheme bg
   bspc query -N -n focused.fullscreen \
     && bspc node -t ~fullscreen
 
   bspc desktop -l tiled
-  bspc config window_gap $(theme -q bspwm.window-gap)
-  bspc config borderless_monocle false
+  gapt $gapped
 }
 
 state=nop
