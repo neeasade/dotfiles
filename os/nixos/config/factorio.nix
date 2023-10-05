04bfe4defc50e90d262c793dfb2f5a850cd1c4d8
neeasade <neeasade@users.noreply.github.com>
2023-10-05 11:11:16 -0500

    factorio server update
    mod time

--- os/nixos/config/factorio.nix
+++ os/nixos/config/factorio.nix
@@ -6,29 +6,59 @@
 }: let
   # to see latest version:
   # https://factorio.com/get-download/stable/headless/linux64
-  version = "1.1.87";
-
+  version = "1.1.91";
+  game = "Ilium";
 in {
   services.factorio = {
     enable = true;
-    description = "VIKINGS";
-    game-name = "VIKINGS";
+    description = game;
+    game-name = game;
+    saveName = game; # nb: save game location is /var/lib/factorio/saves
 
-    admins = lib.splitString "\n" (builtins.readFile "/home/neeasade/sync/main/notes/factorio.admins");
-    game-password = builtins.readFile "/home/neeasade/sync/main/notes/factorio.pass";
+    # admins = lib.splitString "\n" (builtins.readFile (shared.home + "factorio.admins"));
+    admins = [ ];
+    game-password = builtins.readFile (shared.home + "factorio.pass");
+
+    mods =
+      let
+        inherit (pkgs) lib;
+
+        modDir = /home/neeasade/factorio-mods;
+
+        # couldn't get this to work:
+        # modDir = builtins.toPath (shared.home + "factorio-mods");
+
+        modList = lib.pipe modDir [
+          builtins.readDir
+          (lib.filterAttrs (k: v: v == "regular"))
+          (lib.mapAttrsToList (k: v: k))
+          (builtins.filter (lib.hasSuffix ".zip"))
+        ];
+
+        modToDrv = modFileName:
+          pkgs.runCommand "copy-factorio-mods" {} ''
+          mkdir $out
+          cp ${modDir + "/${modFileName}"} $out/${modFileName}
+        ''
+          // { deps = []; };
+      in
+        builtins.map modToDrv modList;
+
     nonBlockingSaving = true;
     openFirewall = true;
     loadLatestSave = true;
 
-    # note: save game location is /var/lib/factorio/saves
-    saveName = "planet_of_the_bass";
+    extraSettings = {
+      # cf https://github.com/wube/factorio-data/blob/master/server-settings.example.json
+      only_admins_can_pause_the_game = false;
+    };
 
     package = pkgs.factorio-headless.overrideAttrs (_: {
       inherit version;
       src = pkgs.fetchurl {
         url = "https://factorio.com/get-download/${version}/headless/linux64";
         name = "factorio-headless-${version}.tar.xz";
-        sha256 = "60b3884b6dad1f4c7b30b7ef2b63619ff4a3204ac7fd894cf09d382b349857cc";
+        sha256 = "2288b21afb1d96aa06712a2ae2e31b9c45f0aa4a2e9dec041125d874f0781f6d";
       };
     });
   };
