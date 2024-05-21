eee8f30f60747446de1e442792aeab3abba531cd
neeasade <neeasade@users.noreply.github.com>
2024-05-21 08:56:17 -0500

    fullscreen_toggle.sh: conditions for body doubling window

--- wm/.wm/scripts/interact/fullscreen_toggle.sh
+++ wm/.wm/scripts/interact/fullscreen_toggle.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
+
 gapped=$(iif '[ $(bspc config window_gap) -gt 0 ]')
 
 do_monocle_full() {
@@ -14,19 +15,35 @@ qutebrowser
 libreoffice
 kitty'
 
-  bspc config left_monocle_padding 0
-  bspc config right_monocle_padding 0
-  bspc desktop -l monocle
+  # bspc config left_monocle_padding 0
+  # bspc config right_monocle_padding 0
+  # bspc desktop -l monocle
 
-  if echo "$pad_list" | grep -q "$window_class"; then
-    gapt $gapped
+  # if echo "$pad_list" | grep -q "$window_class"; then
+  if true; then
+
+    bspc config left_monocle_padding 0
+    bspc config right_monocle_padding 0
+    bspc desktop -l monocle
+
+    # gapt $gapped
   else
-    gapt false 0
+    bspc node -t fullscreen
+    # gapt false 0
+  fi
+
+  if [ "$before" = "monocle_slim" ]; then
+    if bspc query -N -n '.floating.!hidden'; then
+      bspc config left_padding "$(( $(bspc config left_padding) + 450 ))"
+      # bspc config left_monocle_padding "$(( $(bspc config left_monocle_padding) + 450 ))"
+    fi
   fi
 }
 
 do_monocle_slim() {
-  ltheme bg &
+  if [ "$before" = "monocle_full" ]; then
+    ltheme bg &
+  fi
 
   (
     looking_at=$(xprop -id "$(bspc query -N -n)" WM_CLASS | awk -F'"' '{print $4}')
@@ -39,7 +56,6 @@ do_monocle_slim() {
   bspc node -t tiled
 
   monocle_pad_width=$(theme -q bspwm-meta.monocle-pad-width)
-
   bspc config left_monocle_padding $monocle_pad_width
   bspc config right_monocle_padding $monocle_pad_width
 
@@ -58,6 +74,10 @@ do_tiled() {
 
   bspc desktop -l tiled
   gapt $gapped
+
+  if bspc query -N -n '.floating.!hidden'; then
+    bspc config left_padding "$(( $(bspc config left_padding) + 450 ))"
+  fi
 }
 
 do_same() {
@@ -75,10 +95,10 @@ else
   bspc node -t tiled
 
   if [ "$(bspc query -T -d | jq -r .layout)" = "monocle" ]; then
-    if [ $(bspc config left_monocle_padding) -gt 0 ]; then
+    if [ $(bspc config right_monocle_padding) -gt 0 ]; then
       state=monocle_slim
     else
-      if [ "$(bspc config left_monocle_padding)" = 0 ]; then
+      if [ "$(bspc config right_monocle_padding)" = 0 ]; then
         state=monocle_full
       fi
     fi
@@ -92,12 +112,18 @@ fi
 
 # rotate
 echo before: $state
+before=$state
 
 if [ -z "$SLIM" ]; then
   case $state in
     monocle_full) state=tiled ;;
+
     tiled) state=monocle_full ;;
     monocle_slim) state=monocle_full ;;
+
+    # tiled) state=fullscreen ;;
+    # monocle_slim) state=fullscreen ;;
+
     fullscreen) state=tiled ;;
   esac
 else
