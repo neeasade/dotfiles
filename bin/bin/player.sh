4559ad8236494bbb372d9134f2ec26c683dd4637
neeasade <neeasade@users.noreply.github.com>
2025-01-08 13:51:01 -0600

    more playerctl stuff

--- bin/bin/player.sh
+++ bin/bin/player.sh
@@ -1,27 +1,17 @@
 #!/usr/bin/env bash
 
-act() {
-  playerctl "$@"
-}
+act() { playerctl "$@" ;}
 
 playing=$(paste <(playerctl -l) <(playerctl -a status) | grep Playing)
 if [ $(wc -l <<< "$playing" ) -gt 1 ]; then
-  # hopefully mpd
   read -r player _ < <(tail -n 1 <<< "$playing")
-  act() {
-    playerctl -p "$player" "$@"
-  }
+  act() { playerctl -p "$player" "$@" ;}
 fi
 
 if [ "$*" = "toggle" ]; then
   set -- play-pause
 elif [ "$1" = "-f" ]; then
-  fstring=$2
-  # from playerctl(1) format strings
-  for i in title album artist status volume position playerName; do
-    fstring=${fstring/\%${i}%/{{${i}\}\}}
-  done
-  set -- metadata -f "$fstring"
+  set -- metadata -f "$2"
 fi
 
 act "$@"
