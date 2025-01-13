694c2a11107c10947af956befe16cbe5cee0abda
neeasade <neeasade@users.noreply.github.com>
2025-01-13 09:35:26 -0600

    player.sh: volume reporting

--- bin/bin/player.sh
+++ bin/bin/player.sh
@@ -4,7 +4,7 @@
 (ns player.sh
   (:require [clojure.string :as string]
             [clojure.java.shell :as shell]
-            [lib.util :refer [sh]]))
+            [lib.util :refer [sh shh]]))
 
 (defn get-players []
   (let [players (->> (interleave (map (fn [p] (keyword (first (string/split p #"\."))))
@@ -28,17 +28,29 @@
     (if mpd-playing? "mpd"
         (name (ffirst players)))))
 
-(let [args (condp = (first *command-line-args*)
+(defn percent [s]
+  (Math/round
+   (* (parse-double s)
+      100)))
+
+(let [
+      cmd (first *command-line-args*)
+      args (condp = cmd
              "toggle" ["play-pause"]
              "-f" ["metadata" "-f" (second *command-line-args*)]
              "volume" ["volume" (second *command-line-args*)]
-             ["play-pause"])]
+             *command-line-args*
+             ;; ["play-pause"]
+             )]
   (println
    (loop [players (get-players)]
      (if (empty? players)
        "No players found!"
-       (let [result (apply shell/sh "playerctl" "-p" (select-player players) args)
+       (let [player (select-player players)
+             result (apply shell/sh "playerctl" "-p" player args)
              success? (zero? (:exit result))]
          (if success?
-           (:out result)
+           (if (= cmd "volume")
+             (percent (shh "playerctl" "-p" player "metadata" "-f" "{{volume}}"))
+             (:out result))
            (recur (rest players))))))))
